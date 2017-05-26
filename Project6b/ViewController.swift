a12c515b117b5d018a1d6c82f4c225d5ea859f58
jslusser <jslusser@gmail.com>
2017-05-26 14:11:11 +0800

    Auto Layout Anchors

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -46,18 +46,31 @@ class ViewController: UIViewController {
         view.addSubview(label5)
         
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-        let metrics = ["labelHeight": 88]
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        
+//        let metrics = ["labelHeight": 88]
+//        
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: metrics, views: viewsDictionary))
+//            
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: metrics, views: viewsDictionary))
+        var previous: UILabel!
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
             
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+            if previous != nil {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor).isActive = true
+            }
+            
+            previous = label
         }
-        
+    }
         
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
 
     override var prefersStatusBarHidden: Bool {
         return true
